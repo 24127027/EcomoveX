20c8552f6edbc8dd3e2a33f8785b69175162cfd4
24127254 <24127254@student.hcmus.edu.vn>
2025-11-20 15:29:25 +0700

    check check

--- backend/routers/storage_router.py
+++ backend/routers/storage_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Path, Query, UploadFile, File, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Optional
 from datetime import datetime
-from database.user_database import get_user_db
+from database.db import get_db
 from services.storage_service import StorageService
 from models.metadata import *
 from schemas.storage_schema import *
@@ -21,7 +21,7 @@ async def get_user_files_metadata(
 @router.get("/files/{blob_name:path}", response_model=FileMetadataResponse, status_code=status.HTTP_200_OK)
 async def get_file_metadata(
     blob_name: str = Path(..., description="The blob name/path of the file"),
-    db: AsyncSession = Depends(get_user_db),
+    db: AsyncSession = Depends(get_db),
     current_user: dict = Depends(get_current_user)
 ):
     return await StorageService.get_file_by_blob_name(db, current_user["user_id"], blob_name)
@@ -31,7 +31,7 @@ async def upload_file(
     category: FileCategory,
     file: UploadFile = File(...),
     bucket_name: Optional[str] = Query(None),
-    db: AsyncSession = Depends(get_user_db),
+    db: AsyncSession = Depends(get_db),
     current_user: dict = Depends(get_current_user)
 ):
     return await StorageService.upload_file(db, file, current_user["user_id"], category, bucket_name)
@@ -40,7 +40,7 @@ async def upload_file(
 async def delete_file(
     blob_name: str = Path(...),
     bucket_name: Optional[str] = Query(None),
-    db: AsyncSession = Depends(get_user_db),
+    db: AsyncSession = Depends(get_db),
     current_user: dict = Depends(get_current_user)
 ):
     return await StorageService.delete_file(db, current_user["user_id"],  blob_name, bucket_name)
