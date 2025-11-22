54eac57f8f056c79b54317f2d57969bee0c2babd
DUY LÊ MINH <24127027@student.hcmus.edu.vn>
2025-11-22 19:35:40 +0700

    Add is_direct_room_between_users method to RoomService and update FriendService to use it

--- backend/services/friend_service.py
+++ backend/services/friend_service.py
@@ -62,7 +62,7 @@ class FriendService:
                     status_code=status.HTTP_404_NOT_FOUND,
                     detail="Friend request not found"
                 )
-            is_direct_room = await RoomService.get_direct_rooms_between_users(db, user_id, friend_id)
+            is_direct_room = await RoomService.is_direct_room_between_users(db, user_id, friend_id)
             if not is_direct_room:
                 await RoomService.create_direct_room(db, user_id, friend_id)
                 
